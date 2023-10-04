7e2199c2af48e562c1e85b6ab1c197b806bfda0a
dev-ahuja <ahuja.devender@gmail.com>
2023-10-04 18:26:34 +0530

    testing

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "my_tf_ec2" {
   ami           = "ami-0c42696027a8ede58"
   count         = 1
   instance_type = "t2.micro"
-  tags          = { "Name" = "myy-machine" }
+  tags          = { "Name" = "my-machine2" }
 
 }
