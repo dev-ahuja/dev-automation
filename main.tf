0bd21ae89d0bd5c78900c910de0c9b879c2e0280
dev-ahuja <ahuja.devender@gmail.com>
2023-11-05 23:35:15 +0530

    updated TF version

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">=1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,6 +13,11 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+backend "s3" {
+  bucket = "my-test-backend-for-tf"
+  key = "path/to/my/key"
+  region = "us-east-1"
+}
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-05c13eab67c5d8861"
