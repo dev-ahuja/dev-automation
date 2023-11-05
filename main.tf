8277d8a0c26c27486322de61d08856c0630d9dcb
dev-ahuja <ahuja.devender@gmail.com>
2023-11-05 18:12:09 +0530

    destroy

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
-# Configure the AWS Provider
-provider "aws" {
-  region  = "us-east-1"
-}
+# # Configure the AWS Provider
+# provider "aws" {
+#   region  = "us-east-1"
+# }
 
 
 # resource "aws_instance" "my_ec2" {
