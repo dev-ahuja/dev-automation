53ee972459bfad4af779c83456d4c8c6292835f7
dev-ahuja <ahuja.devender@gmail.com>
2023-11-07 13:37:45 +0530

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=1.0"
+  required_version = "~>1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,16 +13,18 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-backend "s3" {
-  bucket = "my-test-backend-for-tf"
-  key = "path/to/my/key"
-  region = "us-east-1"
-}
+# backend "s3" {
+#   bucket = "my-test-backend-for-tf"
+#   key = "path/to/my/key"
+#   region = "us-east-1"
+# }
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-05c13eab67c5d8861"
   instance_type = "t2.micro"
+  user_data = file("${path.module}/app1-install.sh")
+
   tags = {
-    name = "my-dev-machine"
+    name = "first-webserver"
   }
 }
